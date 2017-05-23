435a0e94da74259cb27e8143e0a86f19fac002a6
Talos Valorcan <talos@sunless>
2017-05-23 11:16:58 +1000

    args

--- start.py
+++ start.py
@@ -9,13 +9,27 @@ def usage():
   print " [+] usage: %s [directoryName]" % sys.argv[0]
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    usage()
+  try:
+    opt,args = getopt.getopt(sys.argv[1:],"h",["help","repo=","report="])
+  except getopt.GetoptError as err:
+    print " [x] %s" % str(err)
+    sys.exit(0)
+  argRepo = None
+  argReport = None
+  argTarget = None
+  for o,a in opt:
+    if o in ("-h","--help"):
+      usage()
+      sys.exit(0)
+    elif o == "--repo":
+      argRepo = a
+    elif o == "--report":
+      argReport = a
+    elif o == "--target":
+      argTarget = a 
+  parseEngine = lofwyr.Engine()
+  reportObject = parseEngine.review(argRepo)
+  if reportObject.isBuildBreaking():
     sys.exit(1)
   else:
-    parseEngine = lofwyr.Engine()
-    report = parseEngine.review(sys.argv[1])
-    if report.isBuildBreaking():
-      sys.exit(1)
-    else:
-      sys.exit(0)
+    sys.exit(0)
